b220c04bd886c39c28e3de393b98576bb531fc31
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 01:00:34 +0800

    add sf_helpers

---
+++ R/sf_helper.R
@@ -0,0 +1,40 @@
+#' @title sf object geometry column name
+#' @description
+#' Get the geometry column name of an sf object
+#'
+#' @param sfj An `sf` object.
+#'
+#' @return A character.
+#' @export
+#'
+#' @examples
+#' data("income")
+#' st_geometry_name(income)
+#'
+sf_geometry_name = \(sfj){
+  gname = attr(sfj, "sf_column")
+  return(gname)
+}
+
+#' @title sf object geometry type
+#' @description
+#' Get the geometry type of an sf object
+#'
+#'
+#'
+#' @param sfj
+#'
+#' @return A character
+#' @export
+#'
+#' @examples
+#' data("income")
+#' check_geometry_type(income)
+#'
+sf_geometry_type = \(sfj){
+  sfj_type = sfj %>%
+    sf::st_geometry_type() %>%
+    as.character() %>%
+    unique()
+  return(tolower(sfj_type))
+}
