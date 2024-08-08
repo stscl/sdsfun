60dc9e69a1bcd3dd2ae6ff7d45ccb0fd1f4d2a30
SpatLyu <lyu.geosocial@gmail.com>
2024-08-09 00:11:11 +0800

    add .spwt_contiguity function

--- R/spwt.R
+++ R/spwt.R
@@ -1,11 +1,34 @@
 .spwt_contiguity = \(sfj,
+                     order = 1L,
+                     style = 'W',
                      queen = TRUE,
-                     order = 1,
-                     style = 'B',
+                     cumulate = TRUE,
                      zero.policy = TRUE){
   if (!inherits(sfj,'sf')){
     stop('sfj must be an sf object')
   }
 
+  if (!(sf_geometry_type(sfj) %in% c('point','multipoint',
+                                     'polygon','multipolygon'))){
+    stop("Only (multi-)point and (multi-)polygon vector objects are supported")
+  }
 
+  if (sf_geometry_type(sfj) %in% c('point','multipoint')){
+    sfj = sf_voronoi_diagram(sfj)
+  }
+
+  sfj_nb = spdep::poly2nb(sfj,queen = queen)
+  if (order >= 2) {
+    sfj_nb_highorder = spdep::nblag(sfj_nb, order)
+    if (cumulate){
+      sfj_nb = spdep::nblag_cumul(sfj_nb_highorder)
+    } else {
+      sfj_nb = sfj_nb_highorder
+    }
+  }
+
+  sfj_wt = spdep::nb2mat(sfj_nb, style = style,
+                         zero.policy = zero.policy)
+
+  return(sfj_wt)
 }
--- tests/testthat/test-spwt.R
+++ tests/testthat/test-spwt.R
@@ -1,3 +1,3 @@
-test_that("multiplication works", {
+test_that("spwt with contiguity works well", {
   expect_equal(2 * 2, 4)
 })
