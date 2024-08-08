573f650a34a4e47da6f0a1c65ef183f3ef1f007b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 22:47:02 +0800

    add test for sf_geometry_*

--- tests/testthat/test-sf_helpers.R
+++ tests/testthat/test-sf_helpers.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("sf_geometry_name works", {
+  snnu = sf::read_sf(system.file('extdata/snnu.geojson',package = 'sdsfun'))
+  expect_equal(sf_geometry_name(snnu), "geometry")
+})
+
+test_that("sf_geometry_type works", {
+  snnu = sf::read_sf(system.file('extdata/snnu.geojson',package = 'sdsfun'))
+  expect_equal(sf_geometry_type(snnu), "polygon")
 })
