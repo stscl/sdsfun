e07d2e66001ab8f06d0a3d16c3e38cef75e163ec
SpatLyu <lyu.geosocial@gmail.com>
2024-08-09 23:42:28 +0800

    add utils functions

---
+++ R/utils.R
@@ -0,0 +1,36 @@
+#' @title standardization
+#' @description
+#' To calculate the Z-score using variance normalization, the formula is as follows:
+#'
+#' \eqn{Z = \frac{(x - mean(x))}{sd(x)}}
+#'
+#' @param x A numeric vector
+#'
+#' @return A standardized numeric vector
+#' @export
+#'
+#' @examples
+#' standardize_vector(1:10)
+#'
+standardize_vector = \(x){
+  return((x - mean(x,na.rm = TRUE)) / stats::sd(x,na.rm = TRUE))
+}
+
+#' @title normalization
+#'
+#' @param x A continuous numeric vector.
+#' @param to_left (optional) Specified minimum. Default is `0`.
+#' @param to_right (optional) Specified maximum. Default is `1`.
+#'
+#' @return A continuous vector which has normalized.
+#' @export
+#'
+#' @examples
+#' normalize_vector(c(-5,1,5,0.01,0.99))
+#'
+normalize_vector = \(x,to_left = 0,to_right = 1){
+  xmin = range(x,na.rm = TRUE)[1]
+  xmax = range(x,na.rm = TRUE)[2]
+  xnew = (x - xmin) / (xmax - xmin) * (to_right - to_left) + to_left
+  return(xnew)
+}
