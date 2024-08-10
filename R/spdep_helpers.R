2a658ccde834149f23272a7ff1e362cee2b86a08
SpatLyu <lyu.geosocial@gmail.com>
2024-08-10 13:06:39 +0800

    fix error of .spwt_idw

--- R/spdep_helpers.R
+++ R/spdep_helpers.R
@@ -44,6 +44,35 @@ spdep_contiguity_swm = \(sfj,
   return(spdep_wt)
 }
 
+#' @title constructs spatial weight matrices based on distance
+#' @description
+#' Constructs spatial weight matrices based on distance via `spdep` package.
+#' @note
+#' When `kernel` is setting, using kernel function based distance weight.
+#'
+#' @param sfj An sf object.
+#' @param bandwidth (optional) The bandwidth, default is `NULL`.
+#' @param k (optional) The number of nearest neighbours. Default is `NULL`.
+#' @param kernel (optional) The kernel function, can be one of `uniform`,
+#' `triangular`,`quadratic`(`epanechnikov`),`quartic` and `gaussian`. Default is `NULL`.
+#' @param power (optional) Default is `1`. Useful when `kernel` is not provided.
+#' @param style (optional) `style` can take values `W`, `B`, `C`, and `S`. More to see
+#' `spdep::nb2mat()`. Default is `W`.
+#' @param zero.policy (optional)  if `FALSE` stop with error for any empty neighbour sets,
+#' if `TRUE` permit the weights list to be formed with zero-length weights vectors. Default
+#' is `TRUE`.
+#'
+#' @return A matrix
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' pts = read_sf(system.file('extdata/pts.gpkg',package = 'sdsfun'))
+#'
+#' wt1 = spdep_distance_swm(pts, style = 'B')
+#' wt2 = spdep_distance_swm(pts, kernel = 'gaussian')
+#' wt2 = spdep_distance_swm(pts, k = 3, kernel = 'gaussian')
+#'
 spdep_distance_swm = \(sfj,
                        bandwidth = NULL,
                        k = NULL,
@@ -52,6 +81,9 @@ spdep_distance_swm = \(sfj,
                        style = 'W',
                        zero.policy = TRUE){
   if (is.null(kernel)){
-    spdep_wt = inverse_distance_swm(sfj,power)
+    spdep_wt = .spwt_idw(sfj,bandwidth,power,style,zero.policy)
+  } else {
+    spdep_wt = .spwt_kernel_weight(sfj,bandwidth,k,kernel,style,zero.policy)
   }
+  return(spdep_wt)
 }
