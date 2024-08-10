97f5c991edeb0a30e043ea77706e2e6faa1c1f96
SpatLyu <lyu.geosocial@gmail.com>
2024-08-10 12:28:33 +0800

    update inverse_distance_swm function

--- R/spwt.R
+++ R/spwt.R
@@ -55,11 +55,78 @@
     suppressWarnings({sfj = sf::st_centroid(sfj)})
   }
 
-  nb_knn = spdep::knearneigh(sf::st_coordinates(sfj), k = k)
+  coords = sfj %>%
+    sf::st_coordinates() %>%
+    {.[,c('X','Y')]}
+
+  longlat = dplyr::if_else(sf::st_is_longlat(sfj),TRUE,FALSE,FALSE)
+
+  nb_knn = spdep::knearneigh(coords,k = k,longlat = longlat)
   sfj_nb = spdep::knn2nb(nb_knn)
 
   sfj_wt = spdep::nb2mat(sfj_nb, style = style,
                          zero.policy = zero.policy)
+
+  return(sfj_wt)
+}
+
+#' spatial weights with distance kernel function
+#' @noRd
+.spwt_kernel_weight = \(sfj,
+                        bandwidth = NULL,
+                        k = NULL,
+                        kernel = "gaussian",
+                        style = 'W',
+                        zero.policy = TRUE){
+  .check_spwt(sfj)
+
+  if (sf_geometry_type(sfj) %in% c('multipoint','multipolygon')){
+    suppressWarnings({sfj = sf::st_point_on_surface(sfj)})
+  } else if (sf_geometry_type(sfj) == 'polygon') {
+    suppressWarnings({sfj = sf::st_centroid(sfj)})
+  }
+
+  coords = sfj %>%
+    sf::st_coordinates() %>%
+    {.[,c('X','Y')]}
+
+  longlat = dplyr::if_else(sf::st_is_longlat(sfj),TRUE,FALSE,FALSE)
+
+  if (is.null(k)) {k = 1}
+
+  if (is.null(bandwidth)){
+    k1 = spdep::knn2nb(spdep::knearneigh(coords,k = k,longlat = longlat))
+    bandwidth = max(unlist(spdep::nbdists(k1,coords,longlat = longlat)))
+  }
+
+  kernelnb = spdep::dnearneigh(coords, 0, bandwidth, longlat = longlat)
+  kernelnb = spdep::include.self(kernelnb)
+  kerneldist = spdep::nbdists(kernelnb,coords,longlat = longlat)
+
+  switch (kernel,
+          "uniform" = {
+            kw_fun = function(x) x * 0 + 0.5
+          },
+          "triangular" = {
+            kw_fun = function(x) 1 - abs((x/bandwidth))
+          },
+          "epanechnikov" = {
+            kw_fun = function(x) .75*(1-(x/bandwidth)^2)
+          },
+          "quadratic" = {
+            kw_fun = function(x) .75*(1-(x/bandwidth)^2)
+          },
+          "quartic" = {
+            kw_fun = function(x) (15/16)*(1-(x/bandwidth)^2)^2
+          },
+          "gaussian" = {
+            kw_fun = function(x) sqrt(2*pi)*exp((-(x/bandwidth)^2)/2)
+          }
+  )
+
+  kernelwt = lapply(kerneldist, kw_fun)
+  sfj_wt = spdep::nb2mat(kernelwt, glist = kernelwt,
+                         style = style, zero.policy = zero.policy)
 
   return(sfj_wt)
 }
@@ -73,6 +140,9 @@
 #'
 #' @param sfj Vector object that can be converted to `sf` by `sf::st_as_sf()`.
 #' @param power (optional) Default is 1. Set to 2 for gravity weights.
+#' @param bandwidth (optional) When the distance is bigger than bandwidth, the
+#' corresponding part of the weight matrix is set to 0. Default is `NULL`, which
+#' means not use the bandwidth.
 #' @param normalize (optional) Whether to further normalizes the constructed weight.
 #' Default is `FALSE`.
 #'
@@ -87,6 +157,7 @@
 #'
 inverse_distance_swm = \(sfj,
                          power = 1,
+                         bandwidth = NULL,
                          normalize = FALSE){
   .check_spwt(sfj)
 
@@ -100,13 +171,18 @@ inverse_distance_swm = \(sfj,
     sf::st_coordinates() %>%
     {.[,c('X','Y')]}
 
-  is_arc = ifelse(sf::st_is_longlat(sfj),TRUE,FALSE)
-  if (is_arc) {
+  longlat = dplyr::if_else(sf::st_is_longlat(sfj),TRUE,FALSE,FALSE)
+  if (longlat) {
     distij = stats::as.dist(geosphere::distm(coords))
   } else {
     distij = stats::dist(as.data.frame(coords))
   }
   wij = 1 / distij ^ power
+
+  if (!is.null(bandwidth)){
+    wij = apply(wij,1,\(x) ifelse(x >= bandwidth,0,x))
+  }
+
   if (normalize) {
     wij = apply(wij,1,normalize_vector)
   }
