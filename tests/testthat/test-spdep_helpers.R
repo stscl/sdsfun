2d27b94696f95e1d991665305da9d9fbf8819aa8
SpatLyu <lyu.geosocial@gmail.com>
2024-09-04 19:47:38 +0800

    add test

--- tests/testthat/test-spdep_helpers.R
+++ tests/testthat/test-spdep_helpers.R
@@ -41,9 +41,9 @@ test_that("check knn neighbours contiguity for point data in spdep_contiguity_sw
 test_that("check first order inverse distance weights in spdep_distance_swm",
           {
             pts = sf::read_sf(system.file('extdata/pts.gpkg',package = 'sdsfun'))
-            wt = spdep_distance_swm(pts, style = 'B')
+            wt1 = spdep_distance_swm(pts, style = 'B')
             wt2 = inverse_distance_swm(pts)
-            expect_equal(unname(wt[1,2]), wt2[1,2])
+            expect_equal(unname(wt1[1,2]), wt2[1,2])
           }
 )
 
@@ -52,5 +52,16 @@ test_that("check kernel functional distance weights in spdep_distance_swm",
             pts = sf::read_sf(system.file('extdata/pts.gpkg',package = 'sdsfun'))
             wt = spdep_distance_swm(pts, kernel = 'gaussian', k = 6, style = 'B')
             expect_equal(unname(wt[1,2]), 0)
+          }
+)
+
+test_that("check whether the bandwidth setting is valid in spdep_distance_swm",
+          {
+            pts = sf::read_sf(system.file('extdata/pts.gpkg',package = 'sdsfun'))
+            wt1 = spdep_distance_swm(pts, bandwidth = 10000, style = 'B')
+            wt2 = spdep_distance_swm(pts, bandwidth = 10000,
+                                     kernel = 'gaussian', style = 'B')
+            expect_equal(unname(wt1[1,2]),0)
+            expect_equal(unname(wt2[1,2]),0)
           }
 )
