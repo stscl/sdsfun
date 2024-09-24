0cb585982ce05d97a5698ad3b75f995cfde1b30e
SpatLyu <lyu.geosocial@gmail.com>
2024-09-24 09:37:30 +0800

    export DummyVar

--- src/DummyVar.cpp
+++ src/DummyVar.cpp
@@ -2,14 +2,14 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericMatrix DummyVar(IntegerVector x) {
+Rcpp::NumericMatrix DummyVar(Rcpp::IntegerVector x) {
   // Get the number of unique levels
-  IntegerVector levels = sort_unique(x);
+  Rcpp::IntegerVector levels = sort_unique(x);
   int n = x.size();
   int k = levels.size();
 
   // Create an n-row by k-column matrix to store the dummy variables
-  NumericMatrix dummy(n, k);
+  Rcpp::NumericMatrix dummy(n, k);
 
   // Iterate over the input vector x
   for (int i = 0; i < n; ++i) {
