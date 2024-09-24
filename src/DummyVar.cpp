21e8e1ef92f0adfa6249287ba5f672da8c8d692c
SpatLyu <lyu.geosocial@gmail.com>
2024-09-24 10:07:32 +0800

    update src cpp code

--- src/DummyVar.cpp
+++ src/DummyVar.cpp
@@ -1,10 +1,26 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+// Function to return unique elements while preserving the original order
+IntegerVector Runique(IntegerVector x) {
+  std::vector<int> seen;
+  std::vector<int> result;
+
+  for (int i = 0; i < x.size(); i++) {
+    // If the value has not been seen before, add it to the result
+    if (std::find(seen.begin(), seen.end(), x[i]) == seen.end()) {
+      seen.push_back(x[i]);
+      result.push_back(x[i]);
+    }
+  }
+
+  return wrap(result);
+}
+
 // [[Rcpp::export]]
 Rcpp::IntegerMatrix DummyVar(Rcpp::IntegerVector x) {
   // Get the number of unique levels
-  Rcpp::IntegerVector levels = sort_unique(x);
+  Rcpp::IntegerVector levels = Runique(x);
   int n = x.size();
   int k = levels.size();
 
---
+++ src/DummyVar.h
@@ -0,0 +1,9 @@
+#ifndef DummyVar_H
+#define DummyVar_H
+
+#include <Rcpp.h>
+
+Rcpp::IntegerVector Runique(Rcpp::IntegerVector x);
+Rcpp::IntegerMatrix DummyVar(Rcpp::IntegerVector x);
+
+#endif // DummyVar_H
