20e53c40d7978cae1481e4267f1ecaec6ce3cb92
SpatLyu <lyu.geosocial@gmail.com>
2024-09-24 09:34:07 +0800

    add DummyVar src cpp code

--- src/DummyVar.cpp
+++ src/DummyVar.cpp
@@ -1,2 +1,27 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericMatrix DummyVar(IntegerVector x) {
+  // Get the number of unique levels
+  IntegerVector levels = sort_unique(x);
+  int n = x.size();
+  int k = levels.size();
+
+  // Create an n-row by k-column matrix to store the dummy variables
+  NumericMatrix dummy(n, k);
+
+  // Iterate over the input vector x
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < k; ++j) {
+      // Set the corresponding column to 1 if the level matches, else 0
+      if (x[i] == levels[j]) {
+        dummy(i, j) = 1;
+      } else {
+        dummy(i, j) = 0;
+      }
+    }
+  }
+
+  return dummy;
+}
