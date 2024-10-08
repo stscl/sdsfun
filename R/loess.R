987b3fea55819262a5c8e041138ee7af4ac400ca
SpatLyu <lyu.geosocial@gmail.com>
2024-10-08 13:26:57 +0800

    update loess_optnum

--- R/loess.R
+++ R/loess.R
@@ -2,12 +2,10 @@
 #' @description
 #' Function for determining optimal spatial data discretization for individual variables
 #' based on locally estimated scatterplot smoothing (LOESS) model.
-#'
 #' @note
-#' When `increase_rate` is not satisfied by the calculation, `increase_rate*0.5` is used first.
-#' At this time, if `increase_rate*0.5` is not satisfied again, the discrete number corresponding
+#' When `increase_rate` is not satisfied by the calculation, the discrete number corresponding
 #' to the highest `q statistic` is selected as a return.
-#' @note
+#'
 #' Note that `sdsfun` sorts `discnumvec` from smallest to largest and keeps `qvec` in
 #' one-to-one correspondence with `discnumvec`.
 #'
@@ -34,8 +32,8 @@ loess_optnum = \(qvec, discnumvec, increase_rate = 0.05){
   discnumvec = discnumvec[discnumrank]
   loessf = stats::loess(qvec ~ discnumvec)
   loessrate = (loessf$fitted - dplyr::lag(loessf$fitted)) / dplyr::lag(loessf$fitted)
-  increase_rate = ifelse(max(loessrate,na.rm = TRUE) < increase_rate,
-                         increase_rate * 0.5, increase_rate)
+  # increase_rate = ifelse(max(loessrate,na.rm = TRUE) < increase_rate,
+  #                        increase_rate * 0.2, increase_rate)
   lrtbf = tibble::tibble(discnum = discnumvec,
                          qstatistic = qvec,
                          lr = loessrate,
@@ -45,7 +43,7 @@ loess_optnum = \(qvec, discnumvec, increase_rate = 0.05){
   # debug: when no increase_rate is satisfied, the highest Q-statistic is selected
   if (is.na(lrtbf[1,1,drop = TRUE])){
     res = c('discnum' = discnumvec[which.max(qvec)],
-            'increase_rate' = increase_rate)
+            'increase_rate' = 0)
   } else {
     res = c('discnum' = lrtbf[1,1,drop = TRUE],
             'increase_rate' = increase_rate)
